4130c1e3e843cb87416014fb5b0ca391601ee157
NorthKoreanDefector <goodoltim@gmail.com>
2017-02-24 11:01:06 +0000

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+playerGroup = pygame.sprite.Group()
+SolidGroup = pygame.sprite.Group()
+platformGroup = pygame.sprite.Group()
+ExitGroup = pygame.sprite.Group()
+allSprites = pygame.sprite.Group()
